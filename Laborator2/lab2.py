893ee7336e72c15eba6ec34c61ec1255d4502505
bmircea <bmircea23@gmail.com>
2023-10-25 17:48:35 +0300

    lab2

--- Laborator2/lab2.py
+++ Laborator2/lab2.py
@@ -116,13 +116,62 @@ def main6():
     #sines
     sinewave = lambda x, f: 3 * np.sin(2 * np.pi * f * x)
     
-    samples = np.linspace(-1, 1, 30000)
+    samples = np.linspace(-1, 1, 20000)
     
     for i in (7500, 3750, 0):
         plt.plot(samples[:50], sinewave(samples, i)[:50], label=i)
         plt.legend()
         
     plt.show()
+    
+def main7():
+    sinewave = lambda x: np.sin(2 * np.pi * 10 * x) 
+    
+    samples = np.linspace(-1, 1, 1000)
+    sine1 = sinewave(samples)
+    
+    samples2 = np.array([samples[i] for i in range(3, len(samples), 4)])
+    sine2 = sinewave(samples2)
+    
+    samples3 = np.array([samples[i] for i in range(4, len(samples), 4)])
+    sine3 = sinewave(samples3)
+    plt.figure(figsize=(12, 8))
+    plt.plot(samples[:50], sine1[:50], label="initial")
+    plt.plot(samples2[:50], sine2[:50], label="decimated - starting from 1st")
+    plt.plot(samples3[:50], sine3[:50], label="decimated - starting from 2nd")
+    plt.legend()
+    plt.show()
+    
+    
+def main8():
+    samples = np.linspace(-np.pi/2, np.pi/2, 15000)
+    f = 800 #Hz    
+    sine = lambda x: np.sin(2*np.pi*f*x)
+    taylor_a = lambda x: 2*np.pi*f*x
+    pade_a = lambda x: (2*np.pi*f*x - (7*(2*np.pi*f*x)**3)/60)/(1 + ((2*np.pi*f*x)**2)/20)
+
+    sinewave = sine(samples)
+    taylor_a_wave = taylor_a(samples)
+    pade_a_wave = pade_a(samples)
+
+    plt.figure(figsize=(12, 8))
+    
+    plt.subplot(2, 2, 1)
+    plt.plot(samples, sinewave, label="true sine")
+    plt.plot(samples[:50], taylor_a_wave[:50], label="Taylor approx")
+    
+    plt.subplot(2, 2, 2)
+    plt.plot(samples[:50], sinewave[:50], label="true sine")
+    plt.plot(samples[:50], pade_a_wave[:50], label="Pade approx")
+    
+    plt.subplot(2, 2, 3)
+    plt.plot(samples[:50], (sinewave-taylor_a_wave)[:50], label="Error True sine - Taylor")
+    
+    plt.subplot(2, 2, 4)
+    plt.plot(samples[:50], (sinewave-pade_a_wave)[:50], label="Error True sine - Pade")
+
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
-    main6()
+    main8()
